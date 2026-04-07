49f47f5ae81ac7a8051645e088cfd1bee4800196
CountTails <74679703+CountTails@users.noreply.github.com>
2026-04-07 16:19:43 -0700

    [test session-refresh-flow]: add a test case for the successful flow of a user session refresh request

--- pkg/core/auth_test.go
+++ pkg/core/auth_test.go
@@ -12,7 +12,10 @@ package core
  */
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -30,6 +33,7 @@ import (
 var (
 	pingResponse = bson.D{{Key: "ok", Value: 1}}
 	insertOk     = bson.D{{Key: "ok", Value: 1}, {Key: "n", Value: 1}}
+	deleteOk     = bson.D{{Key: "ok", Value: 1}, {Key: "n", Value: 1}}
 	findUserDoc  = bson.A{
 		bson.M{
 			"_id":           bson.NewObjectID(),
@@ -48,6 +52,23 @@ var (
 			{Key: "id", Value: int64(0)},
 			{Key: "ns", Value: "tournabyte.users"},
 			{Key: "firstBatch", Value: findUserDoc},
+		}},
+	}
+	findSessDoc = bson.A{
+		bson.M{
+			"_id":              fmt.Sprintf("%x", sha256.Sum256(bytes.NewBufferString("abcdefg").Bytes())),
+			"not_valid_before": time.Now().UTC().Add(-time.Hour),
+			"not_valid_after":  time.Now().UTC().Add(time.Hour),
+			"authorizes":       bson.NewObjectID(),
+			"rotated":          false,
+		},
+	}
+	findSessionOk = bson.D{
+		{Key: "ok", Value: 1},
+		{Key: "cursor", Value: bson.D{
+			{Key: "id", Value: int64(0)},
+			{Key: "ns", Value: "tournabyte.sessions"},
+			{Key: "firstBatch", Value: findSessDoc},
 		}},
 	}
 )
@@ -71,12 +92,73 @@ func setupWorkingUserCreationContext(t *testing.T) context.Context {
 	return ctx
 }
 
+func setupWorkingSessionRefreshContext(t *testing.T) context.Context {
+	t.Helper()
+
+	m := drivertest.NewMockDeployment(
+		pingResponse,
+		findSessionOk,
+		findUserOk,
+		deleteOk,
+		insertOk,
+	)
+	mockDb, err := dbx.NewMongoConnection(
+		dbx.ConnectionDeployment(m),
+	)
+	require.NoError(t, err)
+
+	ctx, err := mockDb.SetUpSession(context.Background())
+	require.NoError(t, err)
+
+	return ctx
+
+}
+
 func setupWorkingUserCreationWorkspace(t *testing.T) *handlerutil.HandlerWorkspace {
 	t.Helper()
 	space := handlerutil.DefaultWorkspace()
 	body := models.AuthenticationRequest{
 		Email:    "testuser@example.io",
 		Password: "s3cr3tk3y",
+	}
+
+	space.Set(
+		handlerutil.RequestBindings,
+		handlerutil.Bindings{
+			Body: func(a any) error {
+				outVal := reflect.ValueOf(a)
+				if outVal.Kind() != reflect.Pointer || outVal.IsNil() {
+					return handlerutil.ErrNotAddressable
+				}
+
+				valVal := reflect.ValueOf(body)
+				if !valVal.Type().AssignableTo(outVal.Type().Elem()) {
+					return handlerutil.ErrNotAssignable
+				}
+				outVal.Elem().Set(valVal)
+				return nil
+			},
+		},
+	)
+
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.HS256, Key: []byte(`1010101010101010101010101010101010101010101010101010101010101010`)}, nil)
+	require.NoError(t, err)
+	space.Set(authTokenOptionsKey, models.TokenOptions{
+		Issuer:    "testissuer",
+		Subject:   "testsubject",
+		ExpiresIn: time.Minute,
+		Signer:    signer,
+	})
+	space.Set(authSessionOptionsKey, models.SessionOptions{ExpiresIn: time.Hour})
+
+	return &space
+}
+
+func setupWorkingSessionRefreshWorkspace(t *testing.T) *handlerutil.HandlerWorkspace {
+	t.Helper()
+	space := handlerutil.DefaultWorkspace()
+	body := models.SessionRefreshRequest{
+		RefreshToken: "abcdefg",
 	}
 
 	space.Set(
@@ -163,6 +245,31 @@ func TestUserAuthenticationPipeline(t *testing.T) {
 		defer pCancel(nil)
 
 		pIn <- setupWorkingUserCreationWorkspace(t)
+
+		after, ok := <-pOut
+		require.True(t, ok, "Reading value from pipeline exit channel failed")
+		require.NoError(t, after.Get(userAuthorizationResponseKey, &result))
+
+		assert.NotZero(t, result.ID)
+		assert.NotZero(t, result.AccessToken)
+		assert.NotZero(t, result.RefreshToken)
+
+		select {
+		case <-pCtx.Done():
+			require.NoError(t, context.Cause(pCtx))
+		default:
+		}
+	})
+}
+
+func TestSessionRefreshPipeline(t *testing.T) {
+	t.Run("SessionRefreshedSuccessfully", func(t *testing.T) {
+		pCtx, pCancel, pIn, pOut := sessionRefreshPipeline(setupWorkingSessionRefreshContext(t))
+		var result models.AuthenticatedUser
+		defer close(pIn)
+		defer pCancel(nil)
+
+		pIn <- setupWorkingSessionRefreshWorkspace(t)
 
 		after, ok := <-pOut
 		require.True(t, ok, "Reading value from pipeline exit channel failed")
